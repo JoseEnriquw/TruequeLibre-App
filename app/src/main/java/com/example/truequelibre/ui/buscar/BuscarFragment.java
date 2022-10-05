59b78a40c5a6ec0525f9eefe8457715fc56271e3
Laura Elena Srebernich <70862490+laru1288@users.noreply.github.com>
2022-10-04 23:23:39 -0300

    lo que faltaba

---
+++ app/src/main/java/com/example/truequelibre/ui/buscar/BuscarFragment.java
@@ -0,0 +1,38 @@
+package com.example.truequelibre.ui.buscar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.truequelibre.databinding.FragmentBuscarBinding;
+
+
+public class BuscarFragment extends Fragment {
+
+    private FragmentBuscarBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        BuscarViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(BuscarViewModel.class);
+
+        binding = FragmentBuscarBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textBuscar;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
