01364d502bd30bbfd5f3c5eaeb019deede6ef601
Regina laurentino <reginalaurentino54@gmail.com>
2022-11-04 00:03:13 -0300

    entidades faltantes y cards de articulos por categorias

---
+++ app/src/main/java/com/example/truequelibre/AdapterArticulos.java
@@ -0,0 +1,90 @@
+package com.example.truequelibre;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.view.menu.MenuPopupHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterArticulos extends RecyclerView.Adapter <AdapterArticulos.ViewHolderArticulos>{
+
+    private Context context;
+    private List<EPublicaciones> publicaciones;
+
+    public AdapterArticulos(Context context, List<EPublicaciones> publicaciones) {
+        this.context = context;
+        this.publicaciones = publicaciones;
+    }
+
+    @SuppressLint("RestrictedApi")
+    public  static  class ViewHolderArticulos extends RecyclerView.ViewHolder
+    {
+        ImageView imfotoarticulo;
+        ImageView imfotoperfil;
+        TextView tvNombreyApellido;
+        TextView tvNombre;
+        TextView tvDescripcion;
+        Button btnOpciones;
+
+
+        public ViewHolderArticulos(@NonNull View itemView) {
+            super(itemView);
+
+            imfotoarticulo= itemView.findViewById(R.id.ivFotoarticulo);
+            imfotoperfil= itemView.findViewById(R.id.ivFotoPerfilarticulo);
+            tvNombreyApellido=itemView.findViewById(R.id.tvNombreApellidoComentarios);
+            tvNombre=itemView.findViewById(R.id.idtitutloarticulo);
+            tvDescripcion=itemView.findViewById(R.id.iddescripcionarticulo);
+            btnOpciones= (Button) itemView.findViewById(R.id.btnver);
+        }
+    }
+
+    @NonNull
+    @Override
+    public AdapterArticulos.ViewHolderArticulos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.grid_item_articulo,parent,false);
+        return new AdapterArticulos.ViewHolderArticulos(view);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onBindViewHolder(@NonNull AdapterArticulos.ViewHolderArticulos holder, int position) {
+        holder.tvDescripcion.setText(publicaciones.get(position).getDescripcion());
+        holder.tvNombreyApellido.setText(publicaciones.get(position).getIdusuario().getDNI().getNombre());
+        holder.tvNombre.setText(publicaciones.get(position).getTitulo());
+        Picasso.get()
+                .load(publicaciones.get(position).getUrlImg())
+                .into(holder.imfotoarticulo);
+        Picasso.get()
+                .load(publicaciones.get(position).getIdusuario().getUrlImg())
+                .into(holder.imfotoperfil);
+
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return publicaciones.size();
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/example/truequelibre/EUsuario.java
@@ -0,0 +1,75 @@
+package com.example.truequelibre;
+
+public class EUsuario {
+    private Integer idusuario;
+    private EPersona DNI;
+    private String email;
+    private String contrasenia;
+    private Boolean estado;
+    private String urlImg;
+
+    public EUsuario(Integer idusuario, EPersona DNI, String email, String contrasenia, Boolean estado, String urlImg) {
+        this.idusuario = idusuario;
+        this.DNI = DNI;
+        this.email = email;
+        this.contrasenia = contrasenia;
+        this.estado = estado;
+        this.urlImg = urlImg;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public EUsuario(Integer idusuario, EPersona DNI, String email, String contrasenia, Boolean estado) {
+        this.idusuario = idusuario;
+        this.DNI = DNI;
+        this.email = email;
+        this.contrasenia = contrasenia;
+        this.estado = estado;
+    }
+
+    public Integer getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(Integer idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public EPersona getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(EPersona DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
