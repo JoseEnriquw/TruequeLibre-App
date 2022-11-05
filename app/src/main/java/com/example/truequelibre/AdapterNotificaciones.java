527e0a6b70228c530b79cf3257aad6b87678f557
Regina laurentino <reginalaurentino54@gmail.com>
2022-11-05 17:24:17 -0300

    pantalla notificaciones y su adapter

---
+++ app/src/main/java/com/example/truequelibre/AdapterNotificaciones.java
@@ -0,0 +1,78 @@
+package com.example.truequelibre;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.view.menu.MenuPopupHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterNotificaciones extends RecyclerView.Adapter <AdapterNotificaciones.ViewHolderNotificaciones>{
+
+    private Context context;
+    private List<EPublicaciones> publicaciones;
+
+    public AdapterNotificaciones(Context context, List<EPublicaciones> publicaciones) {
+        this.context = context;
+        this.publicaciones = publicaciones;
+    }
+
+    @SuppressLint("RestrictedApi")
+    public  static  class ViewHolderNotificaciones extends RecyclerView.ViewHolder
+    {
+        ImageView imageView;
+        TextView tvNombreyapellido;
+        TextView tvdescripcionulo;
+        TextView tvfechanotificacion;
+
+
+        public ViewHolderNotificaciones(@NonNull View itemView) {
+            super(itemView);
+
+            imageView= itemView.findViewById(R.id.notificacionfotoarticulo);
+            tvNombreyapellido=itemView.findViewById(R.id.notificacionesusurio);
+            tvdescripcionulo=itemView.findViewById(R.id.notificaciondescripcion);
+            tvfechanotificacion=itemView.findViewById(R.id.aceptadosfecha);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public AdapterNotificaciones.ViewHolderNotificaciones onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.gir_item_notificaciones,parent,false);
+        return new AdapterNotificaciones.ViewHolderNotificaciones(view);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onBindViewHolder(@NonNull AdapterNotificaciones.ViewHolderNotificaciones holder, int position) {
+        holder.tvNombreyapellido.setText(publicaciones.get(position).getIdusuario().getDNI().getNombre());
+        holder.tvdescripcionulo.setText(publicaciones.get(position).getDescripcion());
+        holder.tvfechanotificacion.setText("hace 1 dia");
+        Picasso.get()
+                .load(publicaciones.get(position).getIdusuario().getUrlImg())
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return publicaciones.size();
+    }
+
+
+
+}
