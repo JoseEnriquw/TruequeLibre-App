15299a418cef1efee4170a66be7d11d1b402f25e
Regina laurentino <reginalaurentino54@gmail.com>
2022-11-08 20:22:34 -0300

    subir imagenes desde galeria

--- app/src/main/java/com/example/truequelibre/AgregarPublicaciones.java
+++ app/src/main/java/com/example/truequelibre/AgregarPublicaciones.java
@@ -1,17 +1,45 @@
 package com.example.truequelibre;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.truequelibre.Entity.Categoria;
 import com.example.truequelibre.Utils.Apis;
 import com.example.truequelibre.Utils.ICategoriaService;
+import com.example.truequelibre.Utils.PhotoUtils;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,12 +49,31 @@ public class AgregarPublicaciones extends AppCompatActivity {
     ICategoriaService service;
     ArrayAdapter<String> adapter;
     AutoCompleteTextView dropDawnCate;
+    FloatingActionButton photobutton;
+    AlertDialog.Builder builder;
+    private PhotoUtils photoUtils;
+    private AlertDialog _photoDialog;
+    private Uri mImageUri;
+    private static final int ACTIVITY_SELECT_IMAGE = 1020,
+            ACTIVITY_SELECT_FROM_CAMERA = 1040, ACTIVITY_SHARE = 1030;
+
+    //prueba
+    private static final String TAG = "MainActivity";
+    private static final int PICTURE_RESULT = 122 ;
+    private ContentValues values;
+    private Uri imageUri;
+    private ImageView myImageView;
+    private Bitmap thumbnail;
+
+    String imageurl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_publicaciones);
-
+        photobutton = findViewById(R.id.btnAgregarImagenesPublicacion);
+        myImageView = findViewById(R.id.imgvagregarpublicaciones);
+        builder = new AlertDialog.Builder(this);
         service= Apis.getCategoriaService();
         Call<List<Categoria>> call =service.getCategorias();
 
@@ -51,5 +98,19 @@ public class AgregarPublicaciones extends AppCompatActivity {
             }
         });
 
+
+        photobutton.setOnClickListener(view -> mGetContet.launch("image/*"));
+
     }
+    ActivityResultLauncher<String> mGetContet = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if(result !=null){
+                        myImageView.setImageURI(result);
+                    }
+                }
+            }
+    );
 }
