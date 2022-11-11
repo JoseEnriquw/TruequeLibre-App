2c863a34308606c011e5a63bc5149ce6a8bcc6a2
ivanantunezg <105608411+ivanantunezg@users.noreply.github.com>
2022-11-11 01:58:45 -0300

    refrescar publicaciones al eliminar, editar redirige a EditarPublicaciones

--- app/src/main/java/com/example/truequelibre/EditarPublicaciones.java
+++ app/src/main/java/com/example/truequelibre/EditarPublicaciones.java
@@ -3,13 +3,20 @@ package com.example.truequelibre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditarPublicaciones extends AppCompatActivity {
+
+    EditText txtTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_publicaciones);
+
+        txtTitulo = (EditText) findViewById(R.id.txtTituloEditar);
+        Integer idPublicacion = (Integer) getIntent().getSerializableExtra("idPublicacion");
+        txtTitulo.setText(String.valueOf(idPublicacion));
     }
 
 
